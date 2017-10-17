d29ab0b22301437f5241e60a1e76b1118a8031ec
Lisa Mae <lisamae135b7@me.com>
2017-10-17 13:55:21 -0400

    created front page and updated some files

---
+++ public/js/main.js
@@ -0,0 +1,24 @@
+let concertShow = document.querySelector(“.concertShow”)
+
+review = document.querySelector(“.review”)
+
+
+
+
+concertShow.addEventListner(“click”, function() {
+modal.style.display = “block”;
+classImg.src = “”;
+classInfo.innerHTML = “ post a link to a show”;
+})
+
+
+review.addEventListner(“click”, function() {
+modal.style.display = “block”;
+classImg.src = “”;
+classInfo.innerHTML = ‘write your own review”
+
+
+
+document.getElementById("close").addEventListener("click", function () {
+		modal.style.display = "none";
+	});
